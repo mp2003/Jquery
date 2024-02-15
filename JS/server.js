581861a4790e66b579aabb2f2562d0e75442fa5a
mp2003 <m.p6092003@gmail.com>
2024-02-15 12:42:09 +0530

    adding scss

--- JS/server.js
+++ JS/server.js
@@ -1,12 +1,8 @@
 // console.log($);
 
 $(document).ready(function () {
+  const URL = "https://jsonplaceholder.typicode.com/todos";
   console.log($(".home"));
-  setInterval(() => {
-    $(".home").hide(5000);
-    $(".home").show(5000);
-  }, 10000);
-
   function scrollToContainer(containerClass) {
     $("html, body").animate(
       {
@@ -23,4 +19,33 @@ $(document).ready(function () {
   setTimeout(function () {
     scrollToContainer("start-container");
   }, 6000);
+
+  function fetchData() {
+    return $.ajax({
+      url: URL,
+      method: "GET",
+    });
+  }
+  var promise = fetchData();
+  promise
+    .done((data) => {
+      const ajax = $(".ajax-list");
+      const ajaxList = $(".ajax-list li");
+      // console.log(data.length);
+      for (var i = 0; i < 10; i++) {
+        console.log(data[i].completed);
+        const completedClass = data[i].completed ? "completed" : "";
+
+        // Append a new <li> element with the title and class
+        ajax.append(
+          "<li class='" +
+            completedClass +
+            "'>" +
+            JSON.stringify(data[i].title) +
+            "</li>"
+        );
+      }
+    })
+    .fail((error) => console.log(error))
+    .always(() => console.log("promise completed"));
 });
